c811a2f4f1fd4a18c1b20e7e73d851d39b6e435d
Blaine Garrett <bgarrett@rightteam.io>
2019-01-16 14:38:30 -0600

    Setting up basic structure and adding unit tests.

---
+++ tests/exceptions.test.js
@@ -0,0 +1,38 @@
+const exceptions = require('../core/exceptions');
+
+test('PermissionException contains message', () => {
+  const msg = 'You do not have permission to this resource.';
+  try {
+    throw new exceptions.PermissionException(msg);
+  } catch (e) {
+    expect(e.message).toBe(msg);
+  }
+});
+
+
+test('AuthenticationException contains message', () => {
+  const msg = 'Invalid login credentials.';
+  try {
+    throw new exceptions.AuthenticationException(msg);
+  } catch (e) {
+    expect(e.message).toBe(msg);
+  }
+});
+
+test('DoesNotExistException contains message', () => {
+  const msg = 'The requested resource does not exist.';
+  try {
+    throw new exceptions.DoesNotExistException(msg);
+  } catch (e) {
+    expect(e.message).toBe(msg);
+  }
+});
+
+test('ConflictException contains message', () => {
+  const msg = 'Resource with this id already exists.';
+  try {
+    throw new exceptions.ConflictException(msg);
+  } catch (e) {
+    expect(e.message).toBe(msg);
+  }
+});
